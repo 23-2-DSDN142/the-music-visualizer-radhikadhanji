05bcf7fb6dbb5882bd31dd7a11e38f07ef71c1f3
radhikadhanji <radhikadhanji0517@gmail.com>
2023-10-10 11:46:53 +1300

    Added correct song, volumes and words

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 A P5JS framework for running a simple music visualizer, intended as a creative coding I assignment.
 
 Song details: 
-Title = Just Dropped In (To See What Condition My Condition Was In)
-Artist = Kenny Rogers
+Title = I Nandesu
+Artist = Rerulili (25-ji, Nightcord de. version)
--- music_starter.js
+++ music_starter.js
@@ -7,18 +7,15 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textSize(24);
 
   let lighterPurple = color(130, 13, 141);
-  let darkerPurple = color(123, 12, 154);
+  let darkerPurple = color(123, 12, 154); //two colours for the background lerp map
   let backLerpMap = map(drum, 0, 100, 0, 1);
-  let backRectCol = lerpColor(lighterPurple, darkerPurple, backLerpMap);
+  let backRectCol = lerpColor(lighterPurple, darkerPurple, backLerpMap); //lerp colour between these two cols
   fill(backRectCol);
   noStroke();
-  rect(canvasWidth/2, canvasHeight/2, canvasWidth - 80, canvasHeight - 20); 
+  rect(canvasWidth/2, canvasHeight/2, canvasWidth - 80, canvasHeight - 20); //draws the darker background rectangle 
 
 
-
-
-
-  //Objects section
+  //Browser window class
   class BrowserWindow {
     constructor(name, height, width, x, y){
       this.name = name;
@@ -79,17 +76,17 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
         fill(this.mainCol);
         stroke(this.upBarCol);
         strokeWeight(3);
-        rect(this.x, this.y, this.width, this.height);
+        rect(this.x, this.y, this.width + bass/12, this.height + bass/12);
         strokeWeight(1);
 
         //Top bar
         fill(this.upBarCol);
-        rect(this.x, this.y-(this.height/2), this.width, this.height/10);
+        rect(this.x, this.y-(this.height/2), this.width + bass/12, this.height/10 + bass/12);
 
         let outlineCol = color(145, 255, 147);
         stroke(outlineCol);
         strokeWeight(2.5);
-        line(this.x - this.width/2, this.y - this.height/1.83, this.x + this.width/2, this.y - this.height/1.83); //outline at the top
+        line(this.x - this.width/2, this.y - this.height/1.83 - bass/12, this.x + this.width/2, this.y - this.height/1.83 - bass/12); //outline at the top
 
         fill(this.mainCol);
         for(let i = 0; i > 2; i++){
@@ -110,30 +107,36 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
         else if (this.name == 'HeartDisplay'){
           //Adds the heart display to the window, mapped to the channels
+          //I wish to add the neony effect and the wings someday
           stroke(255);
           fill(0);
           strokeWeight(7);
           beginShape();
 
           vertex(this.x, this.y + this.height/2 - 50);
-          bezierVertex(this.x + this.width/2, 300, this.x + 50, this.y - this.width/2 + 50, this.x, this.y -50 ); //right side of heart
+          bezierVertex(this.x + this.width/1.75, 400, this.x + 100, this.y - this.width/2 + 50, this.x, this.y -50 ); //right side of heart
           vertex(this.x, this.y + this.height/2 - 51);
-          bezierVertex(this.x - this.width/2, 300, this.x - 50, this.y - this.width/2 + 50, this.x, this.y - 50); //left side of heart
+          bezierVertex(this.x - this.width/1.75, 400, this.x - 100, this.y - this.width/2 + 50, this.x, this.y - 50); //left side of heart
 
           endShape();
 
           stroke(0);
           strokeWeight(8);
           noFill();
-          line(this.x, this.y + this.height/2 - 60, this.x, this.y - 40); //fills in the gaps
+          line(this.x, this.y + this.height/2 - 60, this.x, this.y - 40); //fills in the middle gap
 
         }
 
         else if (this.name == 'ChatDisplay'){
           //Adds the chat display to the window, mapped to one of the channels
-          noStroke();
-          fill(247, 126, 213);
-          rect(this.x - this.width/2.52, this.y + 8, this.width/5, this.height - 20); //sidebar
+          //Could make the chat progression happen every time the funky little scramble thing happens (i.e. the chat keeps moving)
+          //POV Mizuki and Ena arguing in the gc while Mafuyu and Kanade are busy living the domestic lesbian housewife dream
+          
+            noStroke();
+            fill(247, 126, 213);
+            rect(this.x - this.width/2.52, this.y + 10, this.width/5 + bass/14, this.height - 20); //sidebar
+          
+          
 
           let firstIcon = color(186, 61, 108);
           let secondIcon = color(111, 111, 223);
@@ -144,32 +147,32 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
           circle(this.x - this.width/4.5, this.y - this.height/3, this.width/12); //first icon
           fill(255);
           for(let i = 0; i < 6; i++){
-            square(this.x - this.width/8 + (i * 40), this.y - this.height/3, 10); //square detailing
+            square(this.x - this.width/8 + (i * 40), this.y - this.height/3, 10 + bass/10); //square detailing
           }
 
           fill(secondIcon);
           circle(this.x - this.width/4.5, this.y - this.height/5, this.width/12); //second icon
           fill(255);
           for(let i = 0; i < 6; i++){
-            square(this.x - this.width/8 + (i * 40), this.y - this.height/5, 10); //square detailing
+            square(this.x - this.width/8 + (i * 40), this.y - this.height/5, 10 + bass/10); //square detailing
           }
 
           for(let i = 0; i < 4; i++){
-            square(this.x - this.width/8 + (i * 40), this.y - this.height/8.5, 10); //an additional line underneath
+            square(this.x - this.width/8 + (i * 40), this.y - this.height/8.5, 10 + bass/10); //an additional line underneath
           }
 
           fill(thirdIcon);
           circle(this.x - this.width/4.5, this.y, this.width/12); //third icon
           fill(255);
           for(let i = 0; i < 5; i++){
-            square(this.x - this.width/8 + (i * 40), this.y, 10); //square detailing
+            square(this.x - this.width/8 + (i * 40), this.y, 10 + bass/10); //square detailing
           }
 
           fill(fourthIcon);
           circle(this.x - this.width/4.5, this.y + this.height/5, this.width/12); //fourth icon
           fill(255);
           for(let i = 0; i < 3; i++){
-            square(this.x - this.width/8 + (i * 40), this.y + this.height/5, 10); //square detailing
+            square(this.x - this.width/8 + (i * 40), this.y + this.height/5, 10 + bass/10); //square detailing
           }
           
         }
@@ -187,7 +190,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   let bar = new BrowserWindow('BarDisplay', 200, 300, canvasWidth/7, canvasHeight/4.5);
   bar.display();
 
-  let chat = new BrowserWindow('ChatDisplay', 350, 350, canvasWidth - 200, canvasHeight - 350);
+  let chat = new BrowserWindow('ChatDisplay', 350, 350, canvasWidth - 190, canvasHeight - 350);
   chat.display();
 
   let sky = new BrowserWindow('SkyDisplay', 250, 350, canvasWidth/5, canvasHeight - 175);
